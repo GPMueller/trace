4c7b89abf0789af377efff99c6455c7777ba941d
G. P. Mueller <gpmueller@outlook.com>
2019-12-15 19:16:03 +0100

    Added first test, using catch2.

---
+++ tests/should_throw.cpp
@@ -0,0 +1,16 @@
+#include "catch.hpp"
+
+#include <trace/trace.hpp>
+
+void library_function()
+{
+    trace_throw("throwing for test");
+}
+
+TEST_CASE("State", "[state]")
+{
+    SECTION("State setup")
+    {
+        REQUIRE_THROWS(library_function());
+    }
+}
