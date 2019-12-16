edee30aca6f46b071a8f6e27469c1180030835da
G. P. Mueller <gpmueller@outlook.com>
2019-12-16 21:08:21 +0100

    Added a test of a layered backtrace.

--- tests/should_not_throw.cpp
+++ tests/should_not_throw.cpp
@@ -11,6 +11,40 @@ catch( const std::exception & ex )
     trace::handle( ex );
 }
 
+void throwing_layer()
+{
+    trace::initiate( "initiation point of the backtrace" );
+}
+
+void middle_layer_2() try
+{
+    throwing_layer();
+}
+
+catch( const std::exception & ex )
+{
+    trace::propagate( "mid layer 2 propagation point of the backtrace" );
+}
+
+void middle_layer_1() try
+{
+    middle_layer_2();
+}
+
+catch( const std::exception & ex )
+{
+    trace::propagate( "mid layer 1 propagation point of the backtrace" );
+}
+
+void top_layer() try
+{
+    middle_layer_1();
+}
+catch( const std::exception & ex )
+{
+    trace::handle( ex );
+}
+
 TEST_CASE( "Should not throw", "[nothrow]" )
 {
     SECTION( "nothrow" )
@@ -19,5 +53,14 @@ TEST_CASE( "Should not throw", "[nothrow]" )
         std::string backtrace = "";
         REQUIRE_NOTHROW( backtrace = trace::latest() );
         REQUIRE_FALSE( backtrace == "" );
+    }
+
+    SECTION( "layered" )
+    {
+        REQUIRE_THROWS( middle_layer_1() );
+        REQUIRE_NOTHROW( top_layer() );
+        std::string backtrace = "";
+        REQUIRE_NOTHROW( backtrace = trace::latest() );
+        REQUIRE_THROWS( throwing_layer() );
     }
 }
